51c8dc1cd445898e6469fbd06c1145b5e99fd7da
Chrono-Inferna <oliver.t.deng@gmail.com>
2020-10-15 10:01:34 -0400

    Sorter Update
    Moved files, created insertion sort

--- Developing/C++/sort/randomVectorBubbleSort.cpp
+++ Developing/C++/sort/randomVectorBubbleSort.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
+#include "randomVectorBubbleSort.hpp"
 
-int main()
+void bubbleSort()
 {
     int vectorElements;
-    bool sorted = false;
     std::vector<int> vec;
 
-    srand(time(NULL));
-
-    std::cout << "How many elements do you want in this array?" << std::endl;
+    std::cout << "How many elements do you want in this vector?" << std::endl
+              << std::endl;
     std::cin >> vectorElements;
+    std::cout << std::endl;
 
     // Generate and display
+    std::cout << "Unsorted Vector:" << std::endl;
     for (int i = 0; i < vectorElements; ++i)
     {
         vec.push_back(std::rand() % 1000);
@@ -22,6 +23,7 @@ int main()
         else
             std::cout << vec[i] << "\0" << std::endl;
     }
+    std::cout << std::endl;
 
     // Sort
     for (int i = 0; i < vec.size(); ++i)
@@ -35,28 +37,30 @@ int main()
                 // Swap variables
                 vec[j] = vec[j] + vec[j + 1];
                 vec[j + 1] = vec[j] - vec[j + 1];
-                vec[j] = vec[j] - vec[j+1];
+                vec[j] = vec[j] - vec[j + 1];
 
                 switched = true;
             }
         }
 
         // If it's sorted, no need to continue
-        if (!switched)
+        if (switched == false)
         {
             break;
         }
-    }
-
-
-    // Display
-    for (int i = 0; i < vectorElements; ++i)
-    {
-        if (i < vectorElements - 1)
-            std::cout << vec[i] << " ";
         else
-            std::cout << vec[i] << "\0" << std::endl;
+        {
+            // Output each sort
+            std::cout << "Sort " << i + 1 << ": " << std::endl;
+            for (int j = 0; j < vec.size(); ++j)
+            {
+                std::cout << vec[j] << " ";
+            }
+            std::cout << std::endl
+                      << std::endl;
+        }
     }
 
-    return 0;
+    std::cout << "The vector has been sorted!" << std::endl
+              << std::endl;
 }
